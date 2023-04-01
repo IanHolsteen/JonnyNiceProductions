9aad0d611843b0f62efaa8522dacf1263e6ad384
JonnyNice <ianholsteen@gmail.com>
2023-04-01 12:49:09 -0700

    fix footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -7,21 +7,20 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer className="bg-zinc-900">
-            <div className="text-white justify-center p-40">
-                <div className="flex ">
-                    <div className="w-96 mr-96">
-                        {/* <h3 className="font-bold text-base mb-4">Sign Up for JonnyNice Newsletter</h3> */}
+            <div className="text-white justify-center p-8">
+                <div className="flex">
+                    <div className="w-96 mr-80">
                         <p className="font-light text-sm mb-5">Sign up to receive exclusive news and discounts on leases.</p>
-                        <Link href="/login"><button className="bg-neutral-600 rounded-full text-sm m-2 py-3 px-10 mb-10">Register</button></Link>
+                        <Link href="/login"><button className="bg-neutral-600 rounded-full text-sm py-3 px-10">Register</button></Link>
                     </div>
                     <div className="w-32"></div>
-                    <h3 className="pl-40 font-bold text-base">Follow
-                        <div className="flex space-x-5 pr-20 pt-8">
-                            {/* <Image src={fb} width="11.25" height="18" alt="Facebook" /> */}
+                    <h3 className="pl-96 font-bold text-base">Follow
+                        <div className="flex space-x-5 pr-20 pt-4">
                             <Link href="https://www.instagram.com/jonnynicebeats/" ><Image src={insta} width="11.25" height="18" alt="Instagram" /></Link>
                             {/* <Image src={twitter} width="18" height="18" alt="Twitter" /> */}
                             <Link href="https://youtube.com/c/jonnynice" ><Image src={youtube} width="11.25" height="18" alt="YouTube" /></Link>
-                        </div></h3>
+                        </div>
+                    </h3>
                 </div>
                 <div className="flex">
                     <ul className="flex mt-10">
