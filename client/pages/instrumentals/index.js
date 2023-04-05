df8a0ce9458b4064634b3dec67359a5942dc5129
JonnyNice <ianholsteen@gmail.com>
2023-04-04 20:38:22 -0700

    more ui;

--- client/pages/instrumentals/index.js
+++ client/pages/instrumentals/index.js
@@ -48,23 +48,23 @@ export default function Instrumentals () {
                 <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]"/>
                 <div className="p-5 text-white z-[2] text-center w-[900px] flex flex-col"  style={{ height: "800px" }} >
                     <h2 className="text-6xl font-bold pt-40">All Beats</h2>
-                    {/* <p className="py-5 text-xl">search</p> */}
+                    <p className="text-l">Select a title to see more options</p>
                     <div className="scroll-container z-[2] flex-grow overflow-y-auto">
                     {instrumentals && instrumentals.map(instrumental => {
                         const audioUrl = `https://jonnynice.onrender.com${instrumental.audio_files[0].file}`;
                         return (
                             <div key={instrumental.id} className="p-4">
                                 <div className="border-2 border-slate-800 rounded-lg p-2">
+                                    <Link href={`/instrumentals/${instrumental.id}`}>
+                                            <p className='underline font-bold text-xl pt-2'>{instrumental.title}</p>
+                                        </Link>
                                     <div className="flex justify-between items-center">
-                                        <button 
+                                        <button
                                             onClick={() => {handleClick(instrumental.audio_files[0].lease?.id)}}
                                             className="border-2 rounded-md"
                                         >
-                                            {selectedInstrumental === instrumental.audio_files[0].lease?.id ? `${instrumental.title} added to cart` : "Add to Cart" }
+                                            {selectedInstrumental === instrumental.audio_files[0].lease?.id ? `${instrumental.title}\u00A0added to cart!` : "Add to Cart" }
                                         </button>
-                                        <Link href={`/instrumentals/${instrumental.id}`}>
-                                            <p className='underline font-bold text-xl p-2'>{instrumental.title}</p>
-                                        </Link>
                                         <h3>Genre: {instrumental.genre.name}</h3>
                                     </div>
                                 <AudioPlayer
@@ -79,10 +79,10 @@ export default function Instrumentals () {
                                 />
                                 </div>
                             </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
     );
 }
