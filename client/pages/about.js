dbc38604f4c0e879cb2a7bf04f12c9711e8c9d34
JonnyNice <ianholsteen@gmail.com>
2023-06-07 09:45:07 -0700

    mobile rendering for about page

--- client/pages/about.js
+++ client/pages/about.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import pic from '../public/images/ian.jpg'
 import Image from "next/image";
 import Layout from '../components/Layout';
 
 export default function About() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkIsMobile = () => {
+            const userAgent = window.navigator.userAgent;
+            setIsMobile(/iPhone|iPad|iPod|Android/i.test(userAgent));
+        };
+
+        checkIsMobile();
+
+        window.addEventListener("resize", checkIsMobile);
+
+        return () => {
+            window.removeEventListener("resize", checkIsMobile);
+        };
+    }, []);
 
     const text = `Producer and songwriter Jonny Nice's sound blends Jazz, Hip Hop, Pop, and RnB.
                     Born in Denver, Colorado, Jonny began playing classical piano at age 6.
@@ -13,11 +29,11 @@ export default function About() {
 
     return(
         <Layout>
-            <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover bg-home">
+            <div className={`flex items-center justify-center h-screen bg-fixed bg-center bg-cover bg-home ${ isMobile ? "pb-10" : ""}`}>
                 <div className="absolute justify-center px-4 top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]"/>
                     <div className="z-[2] h-4/6">
                 <div className="z-[2]">
-                    <div className="p-5 text-white z-[7] text-center items-center w-[900px]">
+                    <div className={`p-5 text-white z-[7] text-center items-center ${ isMobile ? "" : "w-[900px]"}`}>
                 <h2 className="text-6xl font-bold"></h2>
                     <div className="flex justify-center gap-40">
                         <Image className="rounded-lg" alt={pic} src={pic} width={250} height="auto"></Image>
